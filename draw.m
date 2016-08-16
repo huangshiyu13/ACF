b0103bcd7763c8a441203b9e1cdb54d0edc74c8f
Shiyu Huang <huangsy13@gmail.com>
2016-08-16 14:48:26 -0400

    update

---
+++ draw.m
@@ -0,0 +1,31 @@
+clear all
+close all
+p = genpath('../toolbox');
+addpath(p);
+
+fid1=fopen('test.cfg');
+groundtruth = '';
+testFiles={};
+testNames={};
+i = 0;
+while ~feof(fid1)
+    aline=fgetl(fid1);
+    if i == 0,
+        groundtruth = aline;
+    else
+       strs = regexp(aline, ' ', 'split');
+       testFiles{i} = strs{1}; 
+       testNames{i} = strs{2};
+    end
+    i=i+1;
+end
+fclose(fid1);
+pLoad={'lbls',{'person'},'ilbls',{'people'}};
+[gt,dt] = bbGt( 'myLoadAll', groundtruth,testFiles{1},pLoad);
+% thr = 0.5;
+% [gt,dt] = bbGt('evalRes',gt,dt,thr,0);
+% 
+% [fp,tp] = bbGt('compRoc',gt,dt,1);
+% 
+% figure(1); plotRoc([fp tp]);
+% title('roc');
